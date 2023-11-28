a94a625392acf33a77f76610a68b88e78f8f5c91
galgimel <samokhina.galya.15@gmail.com>
2023-11-28 21:18:32 +0300

    Movie Library Start

---
+++ src/test/java/com/library/movie_library/MovieLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.movie_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
